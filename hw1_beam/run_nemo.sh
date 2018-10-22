76f67615d9cdd083c8dfeea4bcd85ed904b9e832
JihoChoi <jihochoi1993@gmail.com>
2018-10-22 17:23:20 +0900

    Add starter code

--- hw1_beam/run_nemo.sh
+++ hw1_beam/run_nemo.sh
@@ -6,10 +6,24 @@ tar -zxvf nemo-shaded.tar.gz
 mvn clean install -Pnemo-runner
 
 #run
+
+: '
+
+# Original
 java -cp `pwd`/nemo-shaded.jar:`yarn classpath`:`pwd`/target/bd_hw-1.0-SNAPSHOT.jar \
 org.apache.nemo.client.JobLauncher \
   -job_id BD_HW_ONE \
   -executor_json `pwd`/nemo-resources.json \
   -optimization_policy org.apache.nemo.compiler.optimizer.policy.DefaultPolicy \
   -user_main edu.snu.bd.examples.Homework \
+  -user_args "--runner=org.apache.nemo.compiler.frontend.beam.NemoPipelineRunner --inputFile=`pwd`/input.txt --output=`pwd`/nemo_output/output"
+'
+
+
+java -cp `pwd`/nemo-shaded.jar:`yarn classpath`:`pwd`/target/bd_hw-1.0-SNAPSHOT.jar \
+org.apache.nemo.client.JobLauncher \
+  -job_id BD_HW_ONE \
+  -executor_json `pwd`/nemo-resources.json \
+  -optimization_policy org.apache.nemo.compiler.optimizer.policy.DefaultPolicy \
+  -user_main edu.snu.bd.hw1.Main \
   -user_args "--runner=org.apache.nemo.compiler.frontend.beam.NemoPipelineRunner --inputFile=`pwd`/input.txt --output=`pwd`/nemo_output/output"
--- hw1_beam/run_spark.sh
+++ hw1_beam/run_spark.sh
@@ -1,4 +1,10 @@
 #install and run
-mvn compile exec:java -Dexec.mainClass=edu.snu.bd.examples.Homework \
+
+# mvn compile exec:java -Dexec.mainClass=edu.snu.bd.examples.Homework \
+#    -Dexec.args="--runner=SparkRunner --inputFile=input.txt --output=`pwd`/spark_output/output" \
+#    -Pspark-runner
+
+
+ mvn compile exec:java -Dexec.mainClass=edu.snu.bd.hw1.Main \
     -Dexec.args="--runner=SparkRunner --inputFile=input.txt --output=`pwd`/spark_output/output" \
     -Pspark-runner
